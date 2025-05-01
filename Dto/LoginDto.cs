2ecd804dba5f1f75f206d3d4d14b5509d10271d0
devabhishek <abhishekdhingra1996@gmail.com>
2025-05-01 22:40:52 +0530

    Second Commit

--- Dto/LoginDto.cs
+++ Dto/LoginDto.cs
@@ -3,7 +3,6 @@
 namespace TaskManagerAPI.Dto {
     public class LoginDto {
             public string Username { get; set; } = string.Empty;
-
             public string Password { get; set; } = string.Empty;
         }
     }
